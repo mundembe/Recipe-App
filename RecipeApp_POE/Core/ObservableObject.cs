40e7568d7b3123fab64f4040345fc4f1ea6a5c89
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-06-14 14:36:30 +0000

    Add project files.

---
+++ RecipeApp_POE/Core/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp_POE.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void onPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
